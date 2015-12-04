88ef09a3db7b41989a5c5819a43cea85b93bb323
wenxuan.zhang@zhan.com <wenxuan.zhang@wenxuanzhangdemacbook-pro.local>
2015-12-04 16:26:57 +0800

    我 页面

--- V2EX/V2EX/Class/Controller/Me(我)/PersonalViewController.swift
+++ V2EX/V2EX/Class/Controller/Me(我)/PersonalViewController.swift
@@ -10,26 +10,43 @@ import UIKit
 
 class PersonalViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet{
+            self.tableView.tableFooterView = UIView()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
+
+// MARK: - UITableViewDelegate & UITableViewDataSource
+extension PersonalViewController {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let topicCell = tableView.dequeueReusableCellWithIdentifier("UserHeaderCell", forIndexPath: indexPath) as? UserHeaderCell {
+    
+            
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+
+    }
+}
+
---
+++ V2EX/V2EX/Class/View/Cell/UserHeaderCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserHeaderCell.swift
+//  V2EX
+//
+//  Created by wenxuan.zhang on 15/12/4.
+//  Copyright © 2015年 张文轩. All rights reserved.
+//
+
+import UIKit
+
+class UserHeaderCell: UITableViewCell {
+
+}
