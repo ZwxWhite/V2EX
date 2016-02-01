896462c8281131f73c215e1a9ec9f687f97a611b
张文轩 <wenxuan.zhang@zhan.com>
2016-02-01 15:25:53 +0800

    获取所有节点

--- V2EX/V2EX/Class/Controller/Node(节点)/NodeViewController.swift
+++ V2EX/V2EX/Class/Controller/Node(节点)/NodeViewController.swift
@@ -7,17 +7,56 @@
 //  节点
 
 import UIKit
-import Crashlytics
+import Alamofire
 
-class NodeViewController: UIViewController {
+class NodeViewController: UITableViewController, Contextualizable {
 
+    
+    var nodes = [V2NodeModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        self.loadNodes()
     }
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+    }
+}
+
+
+extension NodeViewController {
+    
+    private func loadNodes() {
+        request(.GET, v2exBaseUrl+"/api/nodes/all.json", parameters: nil, encoding: .URL, headers: nil).responseJSON { (response) -> Void in
+            switch response.result {
+            case .Success(let responseResult):
+                if let responseArray = responseResult as? NSArray {
+                    for dictionary in responseArray {
+                        let node = V2NodeModel()
+                        node.setupWithDictionary(dictionary as! [String : AnyObject])
+                        self.nodes.append(node)
+                    }
+                }
+                self.tableView.reloadData()
+            case .Failure(let error):
+                V2Error(self.currentDebugContext(),error.description).logError()
+            }
+        }
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.nodes.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCellWithIdentifier("NodeCell") {
+            if let label = cell.contentView.viewWithTag(1001) as? UILabel{
+                label.text = self.nodes[indexPath.row].name
+            }
+            return cell
+        }
+        return UITableViewCell()
     }
 }
---
+++ V2EX/V2EX/Class/Model/V2NodeModel.swift
@@ -0,0 +1,38 @@
+//
+//  V2NodeModel.swift
+//  V2EX
+//
+//  Created by wenxuan.zhang on 16/2/1.
+//  Copyright © 2016年 张文轩. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class V2NodeModel: Object {
+    
+    dynamic var id = 0
+    dynamic var name = ""
+    dynamic var url = ""
+    dynamic var title = ""
+    dynamic var title_alternative = ""
+    dynamic var topics = 0
+    dynamic var header = ""
+    dynamic var footer = ""
+    dynamic var created = 0
+    
+    override class func primaryKey() -> String {
+        return "id"
+    }
+}
+
+
+extension V2NodeModel {
+    func setupWithDictionary(dictionary: [String: AnyObject]) {
+        self.setValuesForKeysWithDictionary(dictionary)
+    }
+    
+    override func setValue(value: AnyObject?, forUndefinedKey key: String) {
+        
+    }
+}
