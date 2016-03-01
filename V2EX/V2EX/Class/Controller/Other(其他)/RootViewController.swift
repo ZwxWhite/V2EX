2d880f7df6df5b78fc70d99a63906509ebbc1e86
张文轩 <wenxuan.zhang@zhan.com>
2016-03-01 13:23:00 +0800

    delete testmodel

--- V2EX/V2EX/Class/Controller/Other(其他)/RootViewController.swift
+++ V2EX/V2EX/Class/Controller/Other(其他)/RootViewController.swift
@@ -13,25 +13,5 @@ class RootViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print(v2Realm.objects(TestModel))
-        
-        try! v2Realm.write { () -> Void in
-            let model = TestModel()
-            model.optionValue = "hello"
-            model.age.value = 20
-            v2Realm.add(model)
-        }
-        
-        print(v2Realm.objects(TestModel))
-
     }
-}
-
-
-class TestModel: Object {
-    
-    dynamic var optionValue: String? = nil
-    
-    let age = RealmOptional<Int>(nil)
 }
